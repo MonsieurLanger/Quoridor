8d1d2e5cf31231257a33796be8b7a47b8f8327b9
Eddine Langer <Eddine Langer@Android>
2017-10-18 17:24:40 +0200

    Merge origin/master into Eddine
    Conflicts:
    	src/Modèle/Case.java

--- src/Graphique/Game_IHM.java
+++ src/Graphique/Game_IHM.java
@@ -5,29 +5,19 @@
  */
 package Graphique;
 
-import Modèle.Case;
-import java.awt.GridBagConstraints;
-import java.awt.image.BufferedImage;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.JFrame;
+
 
 /**
  *
  * @author Eddine Langer
  */
-public class Game_IHM {
-    public static BufferedImage parametre;
-    public static BufferedImage scorebackg;
-   /* public AffichagePlateau jeu;
-    public Grille grille;
-    private Parametres param;
-    private Debut deb;
-    private Options opti;*/
-    /*private Plateau_IHM monPlato;
-    private JLabel win;
-    private JLabel loose;
-    private JPanel panneauJeu;
-    private GridBagConstraints c;
-    public boolean jouable = false;
-    public TableauScore Scoretablo;*/
+public class Game_IHM extends JFrame{
+  private Menu_Accueil accueilPanel;
+  private Menu_Choix partiePanel;
+  private Menu_Configuration configPanel;
+  private Menu_Jeu jeuPanel;
+  private Menu_Joueur joueurPanel;
+  private Menu_Lobby lobbyPanel;
+    
 }
