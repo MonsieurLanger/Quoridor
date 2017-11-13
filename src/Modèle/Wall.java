7174b9f16f2125c4122566812631f55f2267d861
cbl.ho <cbl.ho@HP-CBL.netcenter.clasquin.net>
2017-11-13 17:07:33 +0100

    Ajout classes Abstract et Interface pour Wall Début programmation des classes liées à Wall

--- src/Modèle/Wall.java
+++ src/Modèle/Wall.java
@@ -9,6 +9,17 @@ package Modèle;
  *
  * @author NRMV4488
  */
-public class Wall {
+public class Wall extends AbstractWall{
     
+    
+    public Wall( Color color, Coord coord){
+        super(color, coord);
+    }
+
+    //Vérifie si la barrière peut être placée sur ces cases
+    //On peut additionner les deux coordonnées pour x et les deux pour y (respect de la parité)
+    @Override
+    public boolean isPlaceOk(int xFinal, int yFinal) {
+        return ((this.getX()%2==1 && this.getY()%2==1) || (this.getX()%2==0 && this.getY()%2==0));
+    }
 }
