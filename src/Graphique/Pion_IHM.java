41911d467e1e2097c047fc831bdfd617b0e3d296
MOREL Charles <charles.morel@cpe.fr>
2017-11-13 16:49:30 +0100

    Création de la première ébauche du plateau de jeu

--- src/Graphique/Pion_IHM.java
+++ src/Graphique/Pion_IHM.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Graphique;
 
 /**
  *
- * @author Eddine Langer
+ * @author MOREL Charles
  */
 public class Pion_IHM {
-    
+
 }
